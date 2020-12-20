abdb7d3c7aa3c0bb6c92f36caa5b26c5c20355e4
Parth Pandya <parth0309@gmail.com>
2020-12-21 00:49:40 +0530

    Add log statement and remove import methods | Parth

--- src/main/java/com/sezzle/calculator/controller/CalculatorActivityController.java
+++ src/main/java/com/sezzle/calculator/controller/CalculatorActivityController.java
@@ -5,17 +5,14 @@ import com.sezzle.calculator.exception.BackToFutureException;
 import com.sezzle.calculator.exception.InvalidQuestionAnswerException;
 import com.sezzle.calculator.model.CalculatorActivity;
 import com.sezzle.calculator.service.CalculatorActivityService;
-import org.eclipse.paho.client.mqttv3.MqttException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 import java.util.List;
@@ -44,6 +41,7 @@ public class CalculatorActivityController {
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<CalculatorActivityCO>> findAllLastXActivities() {
+        LOG.info("Fetching the calculator activities through GET method");
         LocalDateTime endTime = LocalDateTime.now().plusMinutes(2L).truncatedTo(ChronoUnit.SECONDS);
         return ResponseEntity.ok(service.findLastXActivitiesLastXMins(endTime));
     }
--- src/main/java/com/sezzle/calculator/service/CalculatorActivityService.java
+++ src/main/java/com/sezzle/calculator/service/CalculatorActivityService.java
@@ -18,7 +18,6 @@ import java.io.IOException;
 import java.time.Clock;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.stream.Collectors;
